9e9b44789b97bf23fe8739a1a051692e0af89b49
Christopher Hoskin <christopher.hoskin@it.ox.ac.uk>
2018-03-07 14:20:21 +0000

    Add a couple of scopes

--- gke.tf
+++ gke.tf
@@ -7,6 +7,8 @@ resource "google_container_cluster" "primary" {
   initial_node_count = 1
   node_config {
     oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
       "https://www.googleapis.com/auth/ndev.clouddns.readwrite"
     ]
   }
