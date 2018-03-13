da11f3d25f9c154cf69e757c654d4da929aa948f
Christopher Hoskin <mans0954@debian.org>
2018-03-13 11:53:46 +0000

    Play around with DNS

--- dns.tf
+++ dns.tf
@@ -1,4 +1,31 @@
+# Set up the tier managed zone
+resource "google_dns_managed_zone" "tier" {
+  name        = "tier"
+  dns_name    = "tier.${var.domain}."
+  description = "tier.${var.domain} DNS zone"
+}
+
+# Reserve an IP address for comanage
 resource "google_compute_address" "comanange-ip" {
   name = "comanage-ip"
+}
+
+# Point comanage.tier.<domain> at the reserved IP
+resource "google_dns_record_set" "comanage" {
+  name = "comanage.${google_dns_managed_zone.tier.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${google_dns_managed_zone.tier.name}"
+
+  rrdatas = ["${google_compute_address.comanange-ip.address}"]
+}
+
+output "name_servers" {
+  value = "${google_dns_managed_zone.tier.name_servers}"
+}
+
+output "ip" {
+  value = "${google_compute_address.comanange-ip.address}"
 }
 
--- gke.tf
+++ gke.tf
@@ -23,24 +23,4 @@ provider "kubernetes" {
   cluster_ca_certificate = "${base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}"
 }
 
-resource "google_dns_managed_zone" "tier" {
-  name        = "tier"
-  dns_name    = "tier.${var.domain}."
-  description = "tier.${var.domain} DNS zone"
-}
 
-/*
-resource "kubernetes_pod" "external-dns" {
-  metadata {
-    name = "external-dns"
-  }
-
-  spec {
-    container {
-      image = "registry.opensource.zalan.do/teapot/external-dns:v0.4.8"
-      name  = "external-dns"
-      args  = ["--source=service","--source=ingress","--domain-filter=tier.${var.domain}","--provider=google","--google-project=${var.project}","--registry=txt","--txt-owner-id=terraform"]
-    }
-  }
-}
-*/
