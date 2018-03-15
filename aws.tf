501ac4cfd7eee5b9633695a067a77d3612f5a86b
Christopher Hoskin <mans0954@debian.org>
2018-03-15 14:34:44 +0000

    Use name_servers from google in AWS NS record

--- aws.tf
+++ aws.tf
@@ -10,3 +10,11 @@ provider "aws" {
 data "aws_route53_zone" "selected" {
   name = "${var.domain}."
 }
+
+resource "aws_route53_record" "tier" {
+  zone_id = "${data.aws_route53_zone.selected.zone_id}"
+  name    = "tier.${data.aws_route53_zone.selected.name}"
+  type    = "NS"
+  ttl     = "300"
+  records = ["${google_dns_managed_zone.tier.name_servers}"]
+}
